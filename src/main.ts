13adbddf2485760b0b785a180dec86703138757d
André Félix <afelixfreitas@gmail.com>
2025-07-06 23:07:46 -0300

    chore: added cookie-parser and helmet packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const whiteListedCors = (process.env.CORS_ALLOWED_ORIGINS || '').split(',');
+
+  app.use(cookieParser());
+  app.use(helmet());
+  app.enableCors({
+    credentials: true,
+    origin: whiteListedCors,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
